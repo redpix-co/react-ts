975b1f302959685286f592815176f4212b47a613
redpix-co <72983943+redpix-co@users.noreply.github.com>
2023-02-07 16:57:31 +0530

    console

--- App.tsx
+++ App.tsx
@@ -2,9 +2,19 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [data, setData] = React.useState('');
+
+  React.useEffect(() => {
+    console.log('useEffect');
+    setData('Welcome');
+  })
+
+  React.useLayoutEffect(() => {
+    console.log('useLayoutEffect');
+  })
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>{data}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
